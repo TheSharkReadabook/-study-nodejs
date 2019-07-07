74312b4c494f9e7f37db9c0c267fe1c0e15156fa
TheSharkReadabook <efride710@gmail.com>
2019-07-08 00:49:39 +0900

    i'm made index page

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ dear visitor,
 
 this project my firest node.js web page.
 
+this web page for creating travel plans.
+
 regards,
 thank you.
---
+++ main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+var express =require('express');
+
+
+
+
+var app = http.createServer(function(request, response){
+    var _url = request.url;
+
+
+    
+    if(_url == '/'){
+
+
+        response.writeHead(200);
+        response.end('_index');
+
+    }else if(_url == '/?id=sign_in'){
+
+
+
+        response.writeHead(200);
+        response.end('success');
+    }
+    
+    
+    else{
+        response.writeHead(404);
+        response.end('failed');
+    }
+
+    
+
+
+
+    
+});
+
+
+app.listen(3000);
