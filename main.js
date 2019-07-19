d84b9869bf4a9e15af3619eb81196e4823b463ad
TheSharkReadabook <efride710@gmail.com>
2019-07-19 13:25:54 +0900

    added ejs

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ var url = require('url');
 var qs = require('querystring');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql');
+// var mysql = require('mysql');
 var express = require('express');
 var app = express();
 
-var dbconfig = require('./db_connect.js');
-var db = mysql.createConnection(dbconfig);
-db.connect();
+// var dbconfig = require('./db_connect.js');
+// var db = mysql.createConnection(dbconfig);
+// db.connect();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 
-    db.query('SELECT view, tit, content FROM board', function(err, topics, fields) {
-    if (!err)
-        console.log('The database result: ', topics);
+    // db.query('SELECT view, tit, content FROM board', function(err, topics, fields) {
+    // if (!err)
+    //     console.log('The database result: ', topics);
 
-    else 
-        console.log('Error while performing Query.', err);
-    });
+    // else 
+    //     console.log('Error while performing Query.', err);
+    // });
 
-    db.end();
+    // db.end();
 
   res.send('Hello World!');
 });
