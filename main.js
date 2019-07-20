593baac21a5e3dfaf2408f9c26166cec57f30746
TheSharkReadabook <efride710@gmail.com>
2019-07-20 23:41:29 +0900

    add ejs and db

--- main.js
+++ main.js
@@ -4,35 +4,45 @@ var url = require('url');
 var qs = require('querystring');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-// var mysql = require('mysql');
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
+var dbconfig = require('./db_connect.js');
+
+var db = mysql.createConnection(dbconfig);
+
+
+//  app.use(express.static('public')); // show static files
 
 app.set('view engine','ejs');  // view engine으로 ejs를 사용하겠다는 의미
+app.set('views','./views'); //views folder
 
-app.set('views','./views');
+db.connect();
+    
+
+    db.query('SELECT num, tit, writer, content FROM board;', function(err, board_list_result, fields) {
+      if (!err){
+          console.log('The database result: ', board_list_result);
+          // console.log('filelds : ',fields);          
+      }
+      else {
+          console.log('Error while performing Query.', err);
+      }
+
+    });
+
+ 
+
 
 app.get('/',function(req,res){
-  res.render('board_list', {title: 'hello'});
+  res.render('index');
 });
 
-    // var dbconfig = require('./db_connect.js');
-    // var db = mysql.createConnection(dbconfig);
-    // db.connect();
-
-    // app.use(express.static('public'));
-
-
-    // db.query('SELECT view, tit, content FROM board', function(err, topics, fields) {
-    // if (!err)
-    //     console.log('The database result: ', topics);
-
-    // else 
-    //     console.log('Error while performing Query.', err);
-    // });
-
-    // db.end();
-
+app.get('/board_list',function(req,res){
+  res.render('board_list', {title: 'hello'});
+});
+    
+db.end();
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
