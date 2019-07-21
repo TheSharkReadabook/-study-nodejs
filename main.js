a2808215a19987d55e76b7bbe29956c765451fce
TheSharkReadabook <efride710@gmail.com>
2019-07-22 01:42:02 +0900

    non

--- main.js
+++ main.js
@@ -15,41 +15,13 @@ var db = mysql.createConnection(dbconfig);
 
 db.connect();
 
-db.once("open", function(){
-  console.log("db connted");
-});
-db.on("error", function(err){
-  console.log("db error", err);
-});
 
 // app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.json());
 
-
-
-var postSchema = mysql.Schema({
-  num: {type:String, require:true},
-  tit: {type:String, require:true},
-  writer: {type:String, require:true}
-});
-
-app.get('/posts', function(req, res){
-  Post.find({}, function(err, posts){
-    if(err) return res.json({success:false, message:err});
-    res.json({success:true, data:posts});
-  });
-});
-
-app.get('/posts', function(req, res){
-  Post.create(req.body.post, function(err, post){
-    if(err) return res.json({success:false, message:err});
-    res.json({success:true, data:post});
-  });
-});
 
 
 app.listen(3000, function () {
