e69c875b5c03104570a82ba5b276c3afe640d03d
TheSharkReadabook <efride710@gmail.com>
2019-07-22 21:41:51 +0900

    index_page_css_

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// var index = require('./routes/index');
+var index = require('./routes/index');
 var member = require('./routes/member');
 var board = require('./routes/board');
 
 
-// app.use('/', index);
+
+app.use('/', index);
 app.use('/member', member);
 app.use('/board', board);
 
@@ -23,25 +24,7 @@ app.use('/board', board);
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
-
-
-// var dbconfig = require('./db_connect.js');
-// var db = mysql.createConnection(dbconfig);
-
-// db.connect();
- 
-
-// app.use(express.static(path.join(__dirname, "public")));
-
-// db.query('SELECT num, tit, writer, content FROM board',
-// function (error, results, fields) {
-//   if (error) throw error;
-
-//   console.log('The solution is: ', results[0].num);
-// });
-// db.end(); 
-
-
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
