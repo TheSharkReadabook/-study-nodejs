e6864aa8e2d5516c51eb8f107d692dc40d9ca849
TheSharkReadabook <efride710@gmail.com>
2019-07-31 13:01:19 +0900

    templete->templates, passport.js

--- routes/member.js
+++ routes/member.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+router.get('/', () => {
+  res.render('member/member')
+})
+
 router.get('/join', function(req, res) {
   res.render('member/join');
 });
@@ -10,5 +13,10 @@ router.post('/members', function(req,res){
  console.log(req);
  res.render('members');
 });
+
+router.get('/login', function(req, res) {
+  res.render('member/login');
+});
+
 
 module.exports = router;
